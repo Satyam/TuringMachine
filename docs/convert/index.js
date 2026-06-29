24d22de65cee485855e87ad50bc154e6ed1b6ea3
Satyam <satyam@satyam.com.ar>
2026-06-29 18:28:15 +0200

    Added lots of explanations to the converter

--- docs/convert/index.js
+++ docs/convert/index.js
@@ -1,10 +1,11 @@
-import { parseTable, generateBytes } from '../src/table2bytes.mjs';
+import { parseTable, generateBytes, bitPatterns } from '../src/table2bytes.mjs';
 
 const $ta = document.getElementById('ta');
 const $btn = document.getElementById('convert');
 const $bytes = document.getElementById('out');
 const $results = document.getElementById('results');
 const $body = document.getElementById('tbody');
+
 $btn.addEventListener('click', (ev) => {
   const table = $ta.value;
   if (table.length === 0) {
@@ -15,16 +16,36 @@ $btn.addEventListener('click', (ev) => {
     console.dir(states);
     const bytes = generateBytes(states);
     $bytes.innerHTML = bytes;
+    try {
+      navigator.clipboard.writeText(bytes);
+    } catch (error) {
+      alert(error.message);
+    }
     const b = [];
     for (const [state, entries] of Object.entries(states)) {
       for (const [symbol, [print, move, next, line, i]] of Object.entries(
         entries
       )) {
+        const byte = bytes[b.length];
         b.push(
-          `<tr><td>${state}</td><td>${symbol}</td><td>${print}</td><td>${move}</td><td>${next}</td></tr>`
+          `<tr>
+            <td>${state}</td>
+            <td>${symbol}</td>
+            <td>${print}</td>
+            <td>${move}</td>
+            <td>${next}</td>
+            <td>000 ${i.toString(2).padStart(6, '0')} ${symbol}</td>
+            <td>${byte.toString(2).padStart(10, '0')}</td>
+            <td>${byte & bitPatterns.dirLeft ? 1 : 0}</td>
+            <td>${byte & bitPatterns.printOne ? 1 : 0}</td>
+            <td>${byte & bitPatterns.move ? 1 : 0}</td>
+            <td>${byte & bitPatterns.write ? 1 : 0}</td>
+            <td>${(byte & bitPatterns.halt).toString(2).padStart(6, '0')}</td>
+          </tr>`
         );
       }
     }
+
     $body.innerHTML = b.join('\n');
     $results.hidden = false;
   } catch (err) {
@@ -36,30 +57,3 @@ $btn.addEventListener('click', (ev) => {
 $ta.addEventListener('keypress', (ev) => {
   $btn.disabled = $ta.value == 0;
 });
-// const table = `
-// ##State  Symbol   Print Operation Next State
-//    s1      0        -       -            Halt
-//    s1      1        0       R            s2
-//    s2      0        0       R            s3
-//    s2      1        1       R            s2
-//    s3      0        1       L            s4
-//    s3      1        1       R            s3
-//    s4      0        0       L            s5
-//    s4      1        1       L            s4
-//    s5      0        1       R            s1
-//    s5      1        1       L            s5
-//    `;
-// const states = parseTable(table);
-// console.dir(states);
-// const bytes = generateBytes(states);
-// console.log(bytes);
-
-// button.addEventListener("click", () => writeClipboardText("<empty clipboard>"));
-
-// async function writeClipboardText(text) {
-//   try {
-//     await navigator.clipboard.writeText(text);
-//   } catch (error) {
-//     console.error(error.message);
-//   }
-// }
